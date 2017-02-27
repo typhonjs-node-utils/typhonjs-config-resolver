42daec19e1c61c79c60f4a2ea2aa2d7a9fee1b2d
typhonrt <support@typhonrt.org>
2017-02-27 09:00:46 -0800

    switched to test eventbus

--- test/src/ConfigResolver.js
+++ test/src/ConfigResolver.js
@@ -1,12 +1,10 @@
-import TyphonEvents  from 'backbone-esnext-events/src/TyphonEvents';
-import { assert }    from 'chai';
-import PluginManager from 'typhonjs-plugin-manager';
+import { testEventbus } from 'backbone-esnext-eventbus';
+import { assert }       from 'chai';
+import PluginManager    from 'typhonjs-plugin-manager';
 
-import testData      from 'typhonjs-config-resolver-tests/testdata';
+import testData         from 'typhonjs-config-resolver-tests/testdata';
 
-const eventbus = new TyphonEvents();
-
-const pluginManager = new PluginManager({ eventbus });
+const pluginManager = new PluginManager({ eventbus: testEventbus });
 
 // Initialize ConfigResolver plugin with no defaults or validation data.
 pluginManager.add({ name: 'extend-resolver', target: './src/ConfigResolver.js' });
@@ -19,19 +17,19 @@ pluginManager.add({
 });
 
 // Uncomment the line below to log resolution chains.
-// eventbus.on('log:info', console.log);
+// testEventbus.on('log:info', console.log);
 
 /** @test {ConfigResolver} */
 describe('ConfigResolver', () =>
 {
    it('throws on no data', () =>
    {
-      assert.throws(() => eventbus.trigger('config:resolver:resolve'));
+      assert.throws(() => testEventbus.trigger('config:resolver:resolve'));
    });
 
    it('allowExtends: false; does not allow extension', () =>
    {
-      const config = eventbus.triggerSync('noextend:config:resolver:resolve', testData[0].tests[0].config);
+      const config = testEventbus.triggerSync('noextend:config:resolver:resolve', testData[0].tests[0].config);
 
       assert.strictEqual(JSON.stringify(config),
        '{"extends":"./node_modules/typhonjs-config-resolver-tests/config/basic.json"}');
@@ -45,7 +43,7 @@ describe('ConfigResolver', () =>
          {
             it(test.title, () =>
             {
-               const config = eventbus.triggerSync('config:resolver:resolve', test.config);
+               const config = testEventbus.triggerSync('config:resolver:resolve', test.config);
 
                // Must delete extends array as it has resolved file names.
                if (test.removeExtends)
